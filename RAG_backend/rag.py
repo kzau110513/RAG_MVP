fb48825e0b696582db414497232da2d59aacbdbf
kzau110513 <sujiayao.hit@gmail.com>
2025-01-19 23:12:18 -0500

    postgre vector

--- RAG_backend/rag.py
+++ RAG_backend/rag.py
@@ -1,5 +1,6 @@
 import os
-from connectDB import get_connection
+# from connectDB import get_connection
+from langchain_postgres import PGVector
 
 # Step 1: Collect Data
 
@@ -12,19 +13,21 @@ for root, dirs, files in os.walk(folder_path):
     for file in files:
         with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
             documents.append((f.name, f.read()))
-print(documents)
+# print(documents)
 #3. Create Embeddings
 
 from langchain_huggingface import HuggingFaceEmbeddings
 # from langchain_openai import OpenAIEmbeddings
 
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+DATABASE_URL = "postgresql+psycopg://postgres:mysecretpassword@localhost:5432/postgres"
 
-# vector_store = PostgresVector(
-#     connection_string=DATABASE_URL,
-#     embedding_dimension=1536,  # Adjust based on your embedding model
-#     table_name="documents"
-# )
+vector_store = PGVector(
+    connection=DATABASE_URL,
+    embeddings=embedding_model,  # Adjust based on your embedding model
+    collection_name="documents",
+    use_jsonb=True,
+)
 
 # vector_store.add_texts(
 #     texts=[doc[1] for doc in documents],
